b63d39ba57d9889dc1dfb1ac92b4308dfe827e7d
Yogesh <yttmlv@gmail.com>
2026-03-30 15:34:22 +0530

    Add Pokemon API fetching functionality with user input handling

--- Day8/hello.py
+++ Day8/hello.py
@@ -3,3 +3,17 @@
 import requests
 
 print ('Welcome to the Pokemon API Fetching!')
+
+while True:
+    pokemon_name = input('Enter the name of a Pokemon (or type "exit" to quit): ')
+    if pokemon_name.lower() == "exit":
+        break
+
+    response = requests.get(f'https://pokeapi.co/api/v2/pokemon/{pokemon_name}')
+    if response.status_code == 200:
+        pokemon_data = response.json()
+        print(f"Name: {pokemon_data['name'].capitalize()}")
+        print(f"Height: {pokemon_data['height']}")
+        print(f"Weight: {pokemon_data['weight']}")
+    else:
+        print("Pokemon not found!")
